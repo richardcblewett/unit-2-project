a329cc1b8e6e591eff3d02a76b77799784a82ddd
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-03 16:16:25 -0600

    Added an empty user profile class

---
+++ src/main/java/com/topmoviesapp/topmovies/model/UserProfile.java
@@ -0,0 +1,12 @@
+package com.topmoviesapp.topmovies.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "profiles")
+public class UserProfile {
+
+
+}
