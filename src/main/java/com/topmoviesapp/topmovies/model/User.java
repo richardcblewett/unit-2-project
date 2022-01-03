8d9acda53f3065dd6d43c3906aa90f183a351497
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-03 16:07:17 -0600

    Added constructor and getter && setters

--- src/main/java/com/topmoviesapp/topmovies/model/User.java
+++ src/main/java/com/topmoviesapp/topmovies/model/User.java
@@ -25,6 +25,39 @@ public class User {
     @JoinColumn(name = "profile_id", referencedColumnName = "id")
     private UserProfile userProfile;
 
+    public User(){
 
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserProfile getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
 }
