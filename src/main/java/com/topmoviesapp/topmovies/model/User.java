dd3ed2c5ec534e7583b2f969ba26a781738b84a6
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-03 16:00:04 -0600

    Added user id

--- src/main/java/com/topmoviesapp/topmovies/model/User.java
+++ src/main/java/com/topmoviesapp/topmovies/model/User.java
@@ -5,6 +5,11 @@ import javax.persistence.*;
 @Entity
 @Table(name="users")
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
 
 }
