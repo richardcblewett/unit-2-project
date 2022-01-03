7bdd339b444eebf6056741b8e0cbbf4f9391d498
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-03 15:09:08 -0600

    Initialize spring and add dependencies

---
+++ src/main/java/com/topmoviesapp/topmovies/TopmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.topmoviesapp.topmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopmoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopmoviesApplication.class, args);
+    }
+
+}
