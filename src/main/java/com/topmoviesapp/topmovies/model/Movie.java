bcc044621d28de279eaa8f33ff0e313beb23d7fe
Richard Blewett <richardcblewett@gmail.com>
2022-01-04 09:01:13 -0500

    added constructors and getters and setters

--- src/main/java/com/topmoviesapp/topmovies/model/Movie.java
+++ src/main/java/com/topmoviesapp/topmovies/model/Movie.java
@@ -16,6 +16,54 @@ public class Movie {
     @Column
     private Long rank;
 
+    public Long getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(Long movieID) {
+        this.movieID = movieID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getRank() {
+        return rank;
+    }
+
+    public void setRank(Long rank) {
+        this.rank = rank;
+    }
+
+    public Long getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Long releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
     @Column
     private Long releaseYear;
 
@@ -30,6 +78,18 @@ public class Movie {
     @Column
     //when a user requests a movie, we want to return the director. so no jsonignore
     private Director director;
+
+    public Movie(Long movieID, String title, Long rank, Long releaseYear, Genre genre, Director director) {
+        this.movieID = movieID;
+        this.title = title;
+        this.rank = rank;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+        this.director = director;
+    }
+
+    public Movie() {
+    }
 
     //TODO later with API
     //@Column
