c778661e63265eae85b4df1d8fbc6a31697240c3
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-04 11:33:43 -0600

    Added oneToMany relationship to Genre and Director Model

--- src/main/java/com/topmoviesapp/topmovies/model/Director.java
+++ src/main/java/com/topmoviesapp/topmovies/model/Director.java
@@ -1,6 +1,10 @@
 package com.topmoviesapp.topmovies.model;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "directors")
@@ -12,6 +16,10 @@ public class Director {
 
     @Column
     private String directorName;
+
+    @OneToMany(mappedBy = "director", orphanRemoval = true)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Movie> movieList;
 
     public Director() {
     }
