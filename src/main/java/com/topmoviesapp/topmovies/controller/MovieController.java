4a9f627befd56cf6c49bfc201e4a13e12d99e952
Richard Blewett <richardcblewett@gmail.com>
2022-01-04 11:48:14 -0500

    added get endpoints to the movie

--- src/main/java/com/topmoviesapp/topmovies/controller/MovieController.java
+++ src/main/java/com/topmoviesapp/topmovies/controller/MovieController.java
@@ -1,4 +1,41 @@
 package com.topmoviesapp.topmovies.controller;
 
+import com.topmoviesapp.topmovies.model.Movie;
+import com.topmoviesapp.topmovies.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+
+@RestController
+@RequestMapping(path = "/api")
 public class MovieController {
+
+    private static final Logger LOGGER = Logger.getLogger(MovieController.class.getName());
+    private MovieService movieService;
+
+    @Autowired
+    public void setMovieService(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    // http://localhost:9092/api/movies
+    @GetMapping("/movies")
+    public List<Movie> getMovies() {
+        LOGGER.info("calling getMovies method from controller");
+        return movieService.getMovies();
+    }
+
+    // http://localhost:9092/api/movies/"{movieId}"
+    @GetMapping(path = "/movies/{movieId}")
+    public Movie getMovie(@PathVariable Long movieId) {
+        LOGGER.info("calling getMovie method from controller");
+        return movieService.getMovie(movieId);
+    }
+
 }
--- src/main/java/com/topmoviesapp/topmovies/service/MovieService.java
+++ src/main/java/com/topmoviesapp/topmovies/service/MovieService.java
@@ -1,4 +1,45 @@
 package com.topmoviesapp.topmovies.service;
 
+import com.topmoviesapp.topmovies.model.Movie;
+import com.topmoviesapp.topmovies.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
 public class MovieService {
+    private MovieRepository movieRepository;
+    private static final Logger LOGGER = Logger.getLogger(MovieService.class.getName());
+
+    @Autowired
+    public void setMovieRepository(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    //get all movies
+    public List<Movie> getMovies() {
+        LOGGER.info("calling getMovies method from service");
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Movie> movies = movieRepository.findByUserProfileId(userDetails.getUser().getId());
+        if (movies.isEmpty()) {
+            throw new InformationMissingException("movies missing for user with id: " + userDetails.getUser().getId());
+        } else {
+            return movies;
+        }
+    }
+
+    //get single movie
+    public Movie getMovie(Long movieId) {
+        LOGGER.info("calling getMovie method from service");
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Movie movie = movieRepository.findByUserProfileIdAndId(userDetails.getUser().getId(), movieId);
+        if (movie != null) {
+            return movie;
+        } else {
+            throw new InformationMissingException("movie with id " + movieId + " does not exist");
+        }
+    }
 }
