ab9a2a9b2d81a5c0de986f3a118a2804b4dc5155
Xander Nguyen <xander.nguyen313@gmail.com>
2022-01-04 10:21:28 -0600

    Added user service

--- src/main/java/com/topmoviesapp/topmovies/controller/UserController.java
+++ src/main/java/com/topmoviesapp/topmovies/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.topmoviesapp.topmovies.controller;
 
+import com.topmoviesapp.topmovies.model.User;
+import com.topmoviesapp.topmovies.model.request.LoginRequest;
+import com.topmoviesapp.topmovies.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/users")
 public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User createUser(@RequestBody User userObject) {
+        return userService.createUser(userObject);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+        return userService.loginUser(loginRequest);
+    }
 }
--- src/main/java/com/topmoviesapp/topmovies/service/UserService.java
+++ src/main/java/com/topmoviesapp/topmovies/service/UserService.java
@@ -1,4 +1,66 @@
 package com.topmoviesapp.topmovies.service;
 
+
+import com.topmoviesapp.topmovies.model.User;
+import com.topmoviesapp.topmovies.model.request.LoginRequest;
+import com.topmoviesapp.topmovies.model.response.LoginResponse;
+import com.topmoviesapp.topmovies.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    
+    public User createUser(User userObject){
+        if(!userRepository.existsByEmailAddress(userObject.getEmailAddress())){
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            // Throw error here
+            // Change error below
+            throw new RuntimeException();
+        }
+    }
+
+    public User findUserByEmailAddress(String email) {
+        return userRepository.findUserByEmailAddress(email);
+    }
+
+
+    // Authenticate user and generate a token
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+        );
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
+        final String jwt = jwtUtils.generateToken(userDetails);
+
+        return ResponseEntity.ok(new LoginResponse(jwt));
+    }
 }
