7db822d64baf33ca98b8322955a4944fa9a09fec
Richard Blewett <richardcblewett@gmail.com>
2022-01-04 11:17:39 -0500

    added security to the application

---
+++ src/main/java/com/topmoviesapp/topmovies/security/JWTRequestFilter.java
@@ -0,0 +1,55 @@
+package com.topmoviesapp.topmovies.security;
+
+import com.topmoviesapp.topmovies.controller.MovieController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Component
+public class JWTRequestFilter extends OncePerRequestFilter {
+    private static final Logger LOGGER = Logger.getLogger(MovieController.class.getName());
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String AuthorizationHeader = httpServletRequest.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (AuthorizationHeader != null && AuthorizationHeader.startsWith("Bearer ")) {
+            jwt = AuthorizationHeader.substring(7);
+            username = jwtUtils.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username);
+            LOGGER.info(userDetails.toString());
+            if (jwtUtils.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
---
+++ src/main/java/com/topmoviesapp/topmovies/security/MyUserDetails.java
@@ -0,0 +1,63 @@
+package com.topmoviesapp.topmovies.security;
+
+import com.topmoviesapp.topmovies.controller.MovieController;
+import com.topmoviesapp.topmovies.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.logging.Logger;
+
+public class MyUserDetails implements UserDetails {
+
+    private static final Logger LOGGER = Logger.getLogger(MovieController.class.getName());
+    private User user;
+    private String userName;
+    private String password;
+    private String emailAddress;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<GrantedAuthority>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        //LOGGER.info("userName is " + user.getUserName());
+        return user.getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser(){
+        return user;
+    }
+}
